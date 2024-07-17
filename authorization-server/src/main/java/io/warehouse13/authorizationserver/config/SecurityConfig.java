0a5bc771709d3666484700d6bf8320a8ec208665
davidvera13 <david.vera@9online.fr>
2024-07-17 22:06:14 +0200

    feat(oauth-server): step 1 - basic configuration

---
+++ authorization-server/src/main/java/io/warehouse13/authorizationserver/config/SecurityConfig.java
@@ -0,0 +1,237 @@
+package io.warehouse13.authorizationserver.config;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+/**
+ * Basic security configuration :
+ * <a href="https://docs.spring.io/spring-authorization-server/reference/getting-started.html">
+ *     https://docs.spring.io/spring-authorization-server/reference/getting-started.html
+ * </a>
+ * After startup:
+ * Open <a href="http://localhost:9000/.well-known/oauth-authorization-server">
+ *     http://localhost:9000/.well-known/oauth-authorization-server</a> to display endpoints
+ * After configuration:
+ * Open <a href="https://oauthdebugger.com/debug">https://oauthdebugger.com/debug</a>
+ * And click on start over
+ */
+@Configuration
+@Slf4j
+@EnableWebSecurity
+public class SecurityConfig {
+
+	/**
+	 * A Spring Security filter chain for the Protocol Endpoints.
+	 * @param http httpSecurity configuration
+	 * @return Security filter chain bean
+	 * @throws Exception exception
+	 */
+	@Bean
+	@Order(1)
+	public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http)
+			throws Exception {
+		log.info("### SecurityConfig -> authorizationServerSecurityFilterChain called");
+		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+		http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+				// Enable OpenID Connect 1.0
+				.oidc(Customizer.withDefaults());
+		http
+				// Redirect to the login page when not authenticated from the authorization endpoint
+				.exceptionHandling((exceptions) -> exceptions
+						.defaultAuthenticationEntryPointFor(
+								new LoginUrlAuthenticationEntryPoint("/login"),
+								new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+						)
+				)
+				// Accept access tokens for User Info and/or Client Registration
+				.oauth2ResourceServer((resourceServer) -> resourceServer
+						.jwt(Customizer.withDefaults()));
+
+		return http.build();
+	}
+
+	/**
+	 * A Spring Security filter chain for authentication.
+	 * @param http httpSecurity configuration
+	 * @return Security filter chain bean
+	 * @throws Exception exception
+	 */
+	@Bean
+	@Order(2)
+	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+			throws Exception {
+		log.info("### SecurityConfig -> defaultSecurityFilterChain called");
+		http
+				.authorizeHttpRequests((authorize) -> authorize
+						.anyRequest().authenticated()
+				)
+				// Form login handles the redirect to the login page from the
+				// authorization server filter chain
+				.formLogin(Customizer.withDefaults());
+
+		return http.build();
+	}
+
+	/**
+	 * An instance of UserDetailsService for retrieving users to authenticate.
+	 * Note: default password decoder is deprecated, it will have to be replaced ASAP
+	 * @return a bean of UserDetailsService
+	 */
+	@Bean
+	@SuppressWarnings("deprecation")
+	public UserDetailsService userDetailsService() {
+		log.info("### SecurityConfig -> userDetailsService called");
+		log.info("### SecurityConfig -> using in memory user details: user / password with role USER");
+		UserDetails userDetails = User
+				.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				//.withUsername("user")
+				//.password("{noop}password")
+				//.authorities("ROLE_USER")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(userDetails);
+	}
+
+	/**
+	 * An instance of RegisteredClientRepository for managing clients.
+	 * @return a RegisteredClientRepository bean to retrieve the configured clients
+	 */
+	@Bean
+	public RegisteredClientRepository registeredClientRepository() {
+		log.info("### SecurityConfig -> registeredClientRepository called");
+		RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+				.clientId("oidc-client")
+				.clientSecret("{noop}secret")
+				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+				// we retrieve an authorization code to be changed to a token
+				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+				//.redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
+				// at start, we will debug the application no url is actually provided
+				.redirectUri("https://oauthdebugger.com/debug")
+				//.postLogoutRedirectUri("http://127.0.0.1:8080/")
+				.scope(OidcScopes.OPENID)
+				.scope(OidcScopes.PROFILE)
+				.clientSettings(clientSettings())
+				.build();
+
+		return new InMemoryRegisteredClientRepository(oidcClient);
+	}
+
+	/**
+	 * Define the client setting: we could have defined it in registeredClientRepository method directly
+	 * @return a client settings configuration
+	 */
+	@Bean
+	public ClientSettings clientSettings() {
+		return ClientSettings.builder()
+				.requireProofKey(true)
+				//.requireAuthorizationConsent(true)
+				.build();
+	}
+
+
+	/**
+	 * An instance of com.nimbusds.jose.jwk.source.JWKSource for signing access tokens.
+	 * @return a JWKSource<SecurityContext> bean
+	 */
+	@Bean
+	public JWKSource<SecurityContext> jwkSource() {
+		log.info("### SecurityConfig -> jwkSource called");
+		KeyPair keyPair = generateRsaKey();
+		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+		RSAKey rsaKey = new RSAKey
+				.Builder(publicKey)
+				.privateKey(privateKey)
+				.keyID(UUID.randomUUID().toString())
+				.build();
+		log.info("public key: \t" + publicKey);
+		log.info("private key: \t" + privateKey.toString());
+		JWKSet jwkSet = new JWKSet(rsaKey);
+		return new ImmutableJWKSet<>(jwkSet);
+	}
+
+	/**
+	 * An instance of java.security.KeyPair with keys generated on startup used to create
+	 * the JWKSource above.
+	 * @return a KeyPair object used by the JWKSource instance
+	 */
+	private static KeyPair generateRsaKey() {
+		log.info("### SecurityConfig -> generateRsaKey called");
+		KeyPair keyPair;
+		try {
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			keyPair = keyPairGenerator.generateKeyPair();
+		}
+		catch (Exception ex) {
+			throw new IllegalStateException(ex);
+		}
+		return keyPair;
+	}
+
+	/**
+	 * An instance of JwtDecoder for decoding signed access tokens.
+	 * @param jwkSource jwkSource
+	 * @return a jwtDecoder bean
+	 */
+	@Bean
+	public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+		log.info("### SecurityConfig -> jwtDecoder called");
+		return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+	}
+
+	/**
+	 * An instance of AuthorizationServerSettings to configure Spring Authorization Server.
+	 * Who provides the authorizations ?
+	 * @return an AuthorizationServerSettings bean
+	 */
+	@Bean
+	public AuthorizationServerSettings authorizationServerSettings() {
+		log.info("### SecurityConfig -> authorizationServerSettings called");
+		return AuthorizationServerSettings.builder()
+				.issuer("http://localhost:9000")
+				.build();
+	}
+
+}
